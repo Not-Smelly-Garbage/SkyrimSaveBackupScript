65a3fd8ff8a2c8ae1a42a5c01911e723d2d17f7b
Not-Smelly-Garbage <62516050+Not-Smelly-Garbage@users.noreply.github.com>
2023-10-14 16:56:30 -0400

    Update AutoBackup.bat

--- Scripts/AutoBackup.bat
+++ Scripts/AutoBackup.bat
@@ -2,9 +2,11 @@
 cd "%HOMEDRIVE%%HOMEPATH%\Documents\My Games\Skyrim Special Edition\Saves"
 set "folderPath=%HOMEDRIVE%%HOMEPATH%\Documents\My Games\Skyrim Special Edition\Saves"
 
-:loop
-echo .
+echo[
 echo Monitoring Save Files...
+echo[
+
+:loop
 set "fileCount=0"
 
 rem Count the number of .ess files in the folder
@@ -12,26 +14,31 @@ for %%f in ("%folderPath%\*.ess") do (
     set /a "fileCount+=1"
 )
 
-rem Check if fileCount is greater than or equal to 16
-if %fileCount% geq 16 (
+echo %fileCount% Saves Detected
+echo[
+
+rem Check if fileCount is greater than or equal to 12
+if %fileCount% geq 12 (
 
     rem Display "Backed Up Saves" with a timecode
     for /f "tokens=2 delims==" %%a in ('wmic os get localdatetime /value') do set "dt=%%a"
     set "currentTime=%dt:~0,4%-%dt:~4,2%-%dt:~6,2% %dt:~8,2%:%dt:~10,2%:%dt:~12,2%"
+    color 20
     echo Backed Up Saves at %currentTime%
+    color
+    echo[
 
-    rem Run the batch file (EDIT THIS IF NEEDED)
+    rem Run the batch file
     call "%HOMEDRIVE%%HOMEPATH%\Documents\My Games\Skyrim Special Edition\Saves\Skyrim Save Backuper-er.bat"
+    timeout /t 5
 
-    echo .
-    echo Monitoring Save Files...
 )
 
 rem Check if SkyrimSE.exe is running
 tasklist | find /i "SkyrimSE.exe" > nul
   if errorlevel 1 (
     echo SkyrimSE.exe is not running. Exiting script.
-    timeout /t 3
+    timeout /t 4
     exit /b
  )
 
